602b595db2705e57c4658d9c79b120552e4ca7b0
June Hanabi <junehanabi@gmail.com>
2020-03-25 20:39:11 -0400

    Brought back an old hack due to a current on-going bug with NuSys trying to be fixed

--- HelloN64/gfx_screen.h
+++ HelloN64/gfx_screen.h
@@ -1,16 +1,21 @@
 #ifndef GFX_SCREEN
 #define GFX_SCREEN
 
-#if defined(NTSC_TV) || defined(MPAL_TV)
-	#define SCREEN_WD   320
-	#define SCREEN_HT   240
-#endif
+// Until a bug with NuSys can be worked out we resort back to the dirty hack of Y-Scaling the output
 
-// For PAL TV we adjust the size accordingly since we use Fullscreen PAL
-#ifdef PAL_TV
-	#define SCREEN_WD   320
-	#define SCREEN_HT   288
-#endif
+//#if defined(NTSC_TV) || defined(MPAL_TV)
+//	#define SCREEN_WD   320
+//	#define SCREEN_HT   240
+//#endif
+//
+//// For PAL TV we adjust the size accordingly since we use Fullscreen PAL
+//#ifdef PAL_TV
+//	#define SCREEN_WD   320
+//	#define SCREEN_HT   288
+//#endif
+
+#define SCREEN_WD   320
+#define SCREEN_HT   240
 
 #define SCREEN_CENTER_X (SCREEN_WD / 2)
 #define SCREEN_CENTER_Y (SCREEN_HT / 2)
--- HelloN64/tv_palTV.c
+++ HelloN64/tv_palTV.c
@@ -4,7 +4,10 @@
 
 void tvPreNmi()
 {
-    
+    // We're dodging around a difficult NuSys bug by doing this for the time being
+    // Turn off the display and re-fix Y-Scale back to 1
+    nuGfxDisplayOff();
+    osViSetYScale(1);
 }
 
 void tvInit()
@@ -13,6 +16,11 @@ void tvInit()
     // 320x288
     // (L)ow-Resolution, (A)nti-Aliasing, (N)on-Interlaced, (1)6-bit
     osViSetMode(&osViModeTable[OS_VI_FPAL_LAN1]);
+
+    // We're dodging around a difficult NuSys bug by doing this for the time being
+    // Squish image slightly
+    // If Pre-NMI event occurs (Reset Button) this will crash if not fixed back correctly
+    osViSetYScale(0.833); // Dirty but works
 }
 
 #endif
